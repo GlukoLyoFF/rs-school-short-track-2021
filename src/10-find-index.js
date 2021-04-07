25336f35f0fbb43d7f309921a1aaf2149ac0a04c
GlukoLyoFF <glukolyoff@gmail.com>
2021-04-07 15:38:23 +0100

    feat: add 10-find-index solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,18 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, val) {
+  let left = 0;
+  let right = array.length;
+  while (right - left > 1) {
+    const mid = parseInt((left + right) / 2, 10);
+    if (array[mid] < val) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+  return (array[left] === val) ? left : right;
 }
 
 module.exports = findIndex;
